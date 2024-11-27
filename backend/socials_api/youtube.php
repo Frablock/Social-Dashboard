cca3caf7abc4d32a959bbec2b0d02b5b738d3d01
Frablock <franon1.frablock29@gmail.com>
2024-11-27 10:29:23 +0100

    Update youtube.php

--- backend/socials_api/youtube.php
+++ backend/socials_api/youtube.php
@@ -1,5 +1,17 @@
 <?php
 include_once "social_api.php";
+/**
+ * Availables methods
+ * 
+ * getFollowersCount(username)
+ * getAllViews(username)
+ * getVideosCount(username)
+ * getProfilePicture(username)
+ * getId(username)
+ * getTitle(username)
+ * 
+ * TODO : Factorize the code
+ */
 
 $env = parse_ini_file('.env');
 
@@ -176,6 +188,88 @@ class youtube { //implements social_api {
             } else {
                 // Process successful response
                 return $data["items"][0]["snippet"]["thumbnails"]["high"]["url"];
+            }
+        }
+    }
+
+    function getId($username) {
+        $ch = curl_init();
+
+        if (!file_exists("save/youtube_".$username.".json")) {
+            curl_setopt_array($ch, [
+                CURLOPT_URL => 'https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&forHandle='.urlencode($username).'&key='.urlencode($this->apiKey),
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_HTTPHEADER => [
+                //    'Authorization: Bearer '.$this->apiKey,
+                    'Accept: application/json'
+                ],
+            ]);
+        } else {
+            curl_setopt_array($ch, [
+                CURLOPT_URL => 'file://' . realpath("save/youtube_".urlencode($username).".json"),
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_HTTPHEADER => [
+                //    'Authorization: Bearer '.$this->apiKey,
+                    'Accept: application/json'
+                ],
+            ]);
+        }
+
+        $response = curl_exec($ch);
+        $err = curl_error($ch);
+
+        if ($err) {
+            echo "cURL Error: " . $err;
+        } else {
+            file_put_contents("save/youtube_".$username.".json", $response);
+            $data = json_decode($response, true);
+            
+            if (isset($data['error'])) {
+                echo "Error: " . $data['error']['message'];
+            } else {
+                // Process successful response
+                return $data["items"][0]["id"];
+            }
+        }
+    }
+
+    function getTitle($username) {
+        $ch = curl_init();
+
+        if (!file_exists("save/youtube_".$username.".json")) {
+            curl_setopt_array($ch, [
+                CURLOPT_URL => 'https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&forHandle='.urlencode($username).'&key='.urlencode($this->apiKey),
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_HTTPHEADER => [
+                //    'Authorization: Bearer '.$this->apiKey,
+                    'Accept: application/json'
+                ],
+            ]);
+        } else {
+            curl_setopt_array($ch, [
+                CURLOPT_URL => 'file://' . realpath("save/youtube_".urlencode($username).".json"),
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_HTTPHEADER => [
+                //    'Authorization: Bearer '.$this->apiKey,
+                    'Accept: application/json'
+                ],
+            ]);
+        }
+
+        $response = curl_exec($ch);
+        $err = curl_error($ch);
+
+        if ($err) {
+            echo "cURL Error: " . $err;
+        } else {
+            file_put_contents("save/youtube_".$username.".json", $response);
+            $data = json_decode($response, true);
+            
+            if (isset($data['error'])) {
+                echo "Error: " . $data['error']['message'];
+            } else {
+                // Process successful response
+                return $data["items"][0]["snippet"]["title"];
             }
         }
     }
